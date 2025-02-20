651fe9011898983282c76e1828e60e61fa7632d0
rcm-webdev <rcm.webdev.pro@gmail.com>
2025-02-20 10:16:35 -0800

    add footer component

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Features from "./components/Features";
 import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Hero />
       <Features />
+      <Footer />
     </div>
   );
 }
